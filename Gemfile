f1210dc9760116c2bec811cbc597e905e2a33f2d
Thiago <thiagodsalles@gmail.com>
2020-08-17 22:45:01 -0300

    add shoulda matchers

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'shoulda-matchers', '~> 4.0'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+require 'support/shoulda_mathcers'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
